9a88c66b19babddcd720c5a2a21f7644bcf749c1
Didier Negretto <didiernegretto96@gmail.com>
2020-01-04 17:11:52 +0100

    Something is wrong somewhere

--- Project_main.m
+++ Project_main.m
@@ -16,7 +16,6 @@ u = [1.5;1;1;1]; %positive yaw + negative pitch + go up
 sim = ode45( @(t,x) quad.f(x,u), [0, Tf], x0);
 quad.plot(sim,u);
 
-
 %% Generate trimmered and linearized version of the quad-copter (TODO 2.1)
 quad = Quad();
 
@@ -33,28 +32,22 @@ sys_transformed = sys*inv(quad.T);           % New system is  A*x + B*inv(T)*v
 
 %% Decompose system (TODO 2.3) 
 
-[sys_x, sys_y, sys_z, sys_yaw] = quad.decompose(sys, xs, us); %sys and not 'sys_transformed
+[sys_x, sys_y, sys_z, sys_yaw] = quad.decompose(sys, xs, us); 
 
 %% DESIGN MPC CONTROLLER FOR EACH SUBSYSTEM (Part 3)
 Ts = 1/5;
 
 % Design MPC controller
-mpc_x   = MPC_Control_x(sys_x, Ts); %Note : each system will be discretized inside the control function
+mpc_x   = MPC_Control_x(sys_x, Ts);
 mpc_y   = MPC_Control_y(sys_y, Ts);
-mpc_z   = MPC_Control_z(sys_z, Ts);
 mpc_yaw = MPC_Control_yaw(sys_yaw, Ts);
+mpc_z   = MPC_Control_z(sys_z, Ts);
 
 %% SIMULATE CONTROLLERS
 clc
 close all
 iters = 5*(20); % 20 seconds simulation.
-sim_controllers(quad, mpc_x,mpc_y,mpc_z,mpc_yaw, Ts, 5);
-
-
-
-%%
-
-
+sim_controllers(quad, mpc_x,mpc_y,mpc_z,mpc_yaw, Ts, 2);
 
 %% FOR FUTURE
 
