35166fe8afd276f1b82ea9bb0cbd241f2776ef70
Didier Negretto <didiernegretto96@gmail.com>
2020-01-07 09:32:29 +0100

    Deliverable_2_1 better

--- Deliverable_2_1.m
+++ Deliverable_2_1.m
@@ -3,18 +3,6 @@ clear all
 close all
 clc
 
-%% Todo 1.2
-quad = Quad();
-Tf = 1.0;
-
-x0 = zeros(12,1);
-%u = [0;1;0;1]; %just go up
-%u = [1;0;1;0]; %positive yaw + go up
-%u = [0;1;0;1]; %negative yaw + go down
-u = [1.5;1;1;1]; %positive yaw + negative pitch + go up
-sim = ode45( @(t,x) quad.f(x,u), [0, Tf], x0);
-quad.plot(sim,u);
-
 %% Generate trimmered and linearized version of the quad-copter (TODO 2.1)
 Ts = 1/5;
 quad = Quad(Ts);
@@ -23,7 +11,7 @@ quad = Quad(Ts);
 sys = quad.linearize(xs, us);               % Linearize the nonlinear model
 
 %% Apply transformation (TODO 2.2) (DELIVERABLE 2.1)
-sys_transformed = sys*inv(quad.T);           % New system is  A*x + B*inv(T)*v
+sys_transformed = sys*inv(quad.T);
 disp(sys.A)
 disp(sys_transformed.A)
 disp(sys.B)
@@ -32,3 +20,9 @@ disp(sys.C)
 disp(sys_transformed.C)
 disp(sys.D)
 disp(sys_transformed.D)
+
+% A,C and D do not change because they are not affected.
+% B in sys has more than 1 non zero element per row, which implies that 
+% more inputs sum up to control the state
+% B in sys_transformed has at max one non-zero element per row, which
+% implies that only one input can control one state
